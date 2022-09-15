a1a36add6f5e45fc1014ddd99dd40c37bf280233
Bodya <kkingqz@gmail.com>
2022-09-15 16:24:19 +0300

    add decor

--- module-9-assistant_bot/main.py
+++ module-9-assistant_bot/main.py
@@ -1,16 +1,31 @@
 import re
 from collections import defaultdict#, namedtuple
 
-def hello(args):
+def input_error(func):
+    def inner(string):
+        try:
+            ret = func(string)
+            return ret
+        except Exception as err:
+            return err
+    return inner
+
+@input_error
+def hello(args_string):
+    if args_string:
+        raise Exception('incorrect args')
     return 'How can I help you?'
 
-def add(args):
+@input_error
+def add(args_string):
+    args = args_string.split()
     if len(args) != 2:
-        raise Exception('too many args')
+        raise Exception('incorrect args')
     elif not (isname(args[0]) and isphone(args[1])):
         raise Exception('incorrect args')
     else:
         update_record(args[0], args[1])
+        return 'user '+args[0]+' added'
 
 def change(args):
     pass
@@ -26,14 +41,6 @@ def quit(print):
         exit()
     else:
         return 'Good bye!'
-
-def input_error(func):
-    def inner(string):
-        try:
-            func(string)
-        except Exception as err:
-            print(err)
-    return inner
 
 def check_input(string):
     return bool(not re.search(r'[^A-Za-z0-9\.\,\s\+]', string))
@@ -52,24 +59,31 @@ def parse_input(string):
     if not check_input(string):
         raise Exception('"'+string+'" not a valid command\nunsupported chars') 
     result = re.sub(r'[\s\,]+', ' ', string.strip().lower())
-    for command in func_dict.keys():
-        if result.startswith(command):
-            return func_dict[command](result.lstrip(command).split())
+    
+    for command in FUNC_DICT:
+        if result.lower().startswith(command):
+            return (command, result[len(command):])#.lstrip(command)#func_dict[command](result.lstrip(command))
     raise Exception(result.split()[0]+' not a valid command')
-    #if len(result) > 4
+    
 
 
 
 def update_record(name, phone):
-    users_dict[name] = phone
+    USERS_DICT[name] = phone
 
-func_dict={'hello': hello, 'add': add, 'change': change, 'phone': phone, 'show all': showall, 'quit': quit}
-users_dict=dict()
+FUNC_DICT={'hello': hello, 'add': add, 'change': change, 'phone': phone, 'show all': showall, 'quit': quit}
+USERS_DICT=dict()
 #Record = namedtuple('Record', ['name', 'phone'])
 
 
 while True:
-    a=input('>>> ')
-    check_input(a)
-    parse_input(a)
-    
+    #a=input('>>> ')
+    #check_input(a)
+    command = parse_input(input('>>> '))
+    if isinstance(command, str) or isinstance(command, Exception):
+        print(command)
+    elif isinstance(command, tuple):
+        print(FUNC_DICT[command[0]](command[1]))
+    else:
+        print('something was wrong')
+    #print (users_dict)
