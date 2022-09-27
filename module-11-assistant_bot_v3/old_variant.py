ebce747ccca3e55d9640ebfd122b6559d6587d12
Bodya <kkingqz@gmail.com>
2022-09-27 19:41:35 +0300

    re writte with some improvement

--- module-11-assistant_bot_v3/old_variant.py
+++ module-11-assistant_bot_v3/old_variant.py
@@ -1,6 +1,6 @@
 import re
-from collections import UserDict, defaultdict
-from datetime import date, datetime
+from collections import UserDict, UserString, defaultdict
+from datetime import date, datetime, timedelta
 
 try:
     import dateutil.parser as Dateparse
@@ -16,23 +16,15 @@ except ModuleNotFoundError:
             except ValueError:
                 raise Dateparse._parser.ParserError
 
-
-class Field():
+class Field(UserString):
     def __init__(self, seq: object) -> None:
+        super().__init__(seq)
         self.__value = None
+        self.data = None
         self.value = seq
 
     def __format__(self, __format_spec: str) -> str:
         return str.__format__(self.value, __format_spec)
-
-    def __str__(self) -> str:
-        return self.__value
-
-    def lower(self):
-        return str.lower(self.__value)
-
-    def upper(self):
-        return str.upper(self.__value)
         
     @property
     def value(self):
@@ -41,7 +33,20 @@ class Field():
     def value(self, string):
         self.__value = string
 
+#убираем стандартное поле data. наследование от userstring, необходимо для того что б работали стандартные str методы lower, upper и т д
+    @property
+    def data(self):
+        return self.value
+    @data.setter
+    def data(self, value):
+        pass
+#----------------------------------------------
+
 class Phone(Field):
+    #def __init__(self, seq: object) -> None:
+    #    super().__init__(seq)
+    #    self.value = self.value.replace(' ','').replace('(','').replace(')','').replace('+','').replace('-','')
+    
     def sanitize_phone_number(self, string):
         return string.replace(' ','').replace('(','').replace(')','').replace('+','').replace('-','')       
 
@@ -77,7 +82,7 @@ class Birthday(Field):
             self.__value = Dateparse.parse(string).date()
         except Dateparse._parser.ParserError:
             raise ValueError('incorrect date format. enter yyyy-mm-dd')
-
+            
 class Record():
     DEFAULT_FIELDS = {'phone': Phone, 'email': EMail, 'birthday': Birthday}
 
@@ -85,30 +90,51 @@ class Record():
         self.name = Name(name)
         self.data = defaultdict(list)
 
+        self.phones = self.data['phone']
+        self.emails = self.data['email']
+
         if field_type and values:
             self.add_field(field_type, values)
-   
-    def get_field_type(self, field_type):
-        return Record.DEFAULT_FIELDS.get(field_type, Field)
 
+    def __iter__(self):
+        self.n = -1
+        return self
+
+    def __next__(self):
+        keys = list(self.data.keys())
+        if self.n < len(keys)-1:
+            self.n += 1
+            return keys[self.n]
+        else:
+            self.n = -1
+            raise StopIteration
+   
     def add_field(self, field_type, values):
         if field_type == 'birthday' and ((len(values) > 1) or self.data[field_type]):
             raise ValueError('only one birthday available')
 
-        self.data[field_type].extend(map(lambda v: self.get_field_type(field_type)(v), values))
+        self.data[field_type].extend(map(lambda v: Record.DEFAULT_FIELDS.get(field_type, Field)(v), values))
         return f'added {field_type} for contact: {self.name}'
 
-    def change_field(self, field_type, oldvalue, newvalue):
-        index = self.data[field_type].index(self.get_field_type(field_type)(oldvalue))
-        self.data[field_type][index] = self.get_field_type(field_type)(newvalue)
+    def change_field(self, field_type, oldvalue, newvalue):#oldphone, newphone):
+        try:
+            index = self.data[field_type].index(Record.DEFAULT_FIELDS.get(field_type, Field)(oldvalue))
+        except ValueError:
+            raise ValueError(f'{field_type} not found') 
+        self.data[field_type][index] = Record.DEFAULT_FIELDS.get(field_type, Field)(newvalue)
         return f'{field_type} change'
 
     def remove_field(self, field_type, value):
-        index = self.data[field_type].index(self.get_field_type(field_type)(value))
+        try:
+            index = self.data[field_type].index(Record.DEFAULT_FIELDS.get(field_type, Field)(value))
+        except ValueError:
+            raise ValueError(f'{field_type} not found') 
         self.data[field_type].pop(index)
         return f'{field_type} removed'
 
     def remove_field_type(self, field_type):
+        if not field_type in self.data:
+            raise ValueError(f'{field_type} not found')
         self.data.pop(field_type)
         return f'{field_type}s removed'
 
@@ -117,7 +143,7 @@ class Record():
         current_year_bd = self.data['birthday'][0].birthday_current_year
         days = str((current_year_bd - today).days)
         return f'today is {today}. {self.name} birthday day in current year - {current_year_bd}\n{days} day(s)'
-
+        
 class AddressBook(UserDict):
     def add_record(self, name, field_type='', *values):
         if name not in self.data:
@@ -127,11 +153,17 @@ class AddressBook(UserDict):
         return self.data[name].add_field(field_type, values)
 
     def remove_record(self, name):
-        self.data.pop(name)
-        return f'contact {name} removed'
+        try:
+            self.data.pop(name)
+            return f'contact {name} removed'
+        except KeyError:
+            raise KeyError(f'contact {name} not found')
 
     def change_record_field(self, name, field_type, oldvalue, newvalue):
-        return self.data[name].change_field(field_type, oldvalue, newvalue)
+        try:
+            return self.data[name].change_field(field_type, oldvalue, newvalue)
+        except KeyError:
+            raise KeyError(f'contact {name} not found')
 
     def show(self, name, field_type=''):
         maxlen=80
@@ -144,7 +176,7 @@ class AddressBook(UserDict):
                 result += ('|  {:^10}  |  {:^'+str(maxlen-21)+'}  |\n').format(f'{field_type} '+str(count+1), value)
                 result += '-'*maxlen+'\n'
         else:
-            for field_type in self.data[name].data:
+            for field_type in self.data[name]:
                 for count, value in enumerate(self.data[name].data[field_type]):
                     result += ('|  {:^10}  |  {:^'+str(maxlen-21)+'}  |\n').format(f'{field_type} '+str(count+1), value)
                     result += '-'*maxlen+'\n'
@@ -180,94 +212,155 @@ class AddressBook(UserDict):
             result += self.show(contact)+'\n'
         return result
 
+
+
+
 def input_error(func):
     def inner(string):
         try:
+            #print('i_e')
             ret = func(string)
             return ret
         except (KeyError, ValueError, IndexError) as err:
-            print(func.__name__)
-            if func.__name__ in ('change', 'phone', 'show', 'remove', 'birthday'):
-                return 'record not found'
             return err
     return inner
 
-def processing_args(args_string, args_num_min, args_num_max, help_string=''):
-    args = args_string.split()
-
-    try:
-        if args[0] == 'help':
-            raise ValueError(help_string)
-    except IndexError:
-        pass
-
-    if args_num_max == 0 and args:
-        raise ValueError('incorrect args\nuse without args\n'+help_string)
-
-    if not (args_num_min <= len(args) <= args_num_max):
-        raise ValueError('incorrect number of arguments\n'+help_string)
-
-    if len(args) > 0:
-        if re.search(r'[^A-Za-z]', args[0]):
-            raise ValueError('incorrect name')
-
-    return args
+def input_args(func):
+    def inner(string):
+        #print(func.__annotations__)
+        return func(string)
+    return inner
 
 @input_error
+@input_args
 def hello(args_string):
-    args = processing_args(args_string, 0, 0)
+    if args_string:
+        raise ValueError('incorrect args\nuse "hello" without args')
     return 'How can I help you?'
 
 @input_error
 def add(args_string):
-    args = processing_args(args_string, 3, 99, 'use:\n   add <name> <field type> <value> [<value2>, ...]')    
-    return contacts.add_record(*args)
+    args = args_string.split()
+
+    if args[0] == 'help':
+        return 'use:\n   add <name> <phone> or add <name> <field type> <value> [<value2>, ...]'
+    
+    if len(args) < 2:
+        raise ValueError('incorrect args\nplease enter: add <name> <phone> or add <name> <field type> <value> [<value2>, ...]')
+
+    if not isname(args[0]):
+        raise ValueError('incorrect name')
+
+    if len(args) == 2:
+        return contacts.add_record(args[0], 'phone', args[1])
+    
+    return contacts.add_record(args[0], args[1], *args[2:])
 
 @input_error
 def change(args_string):
-    args = processing_args(args_string, 4, 4, 'use:\n   change <name> <field type> <old value> <new value>')    
+    args = args_string.split()
+
+    if args[0] == 'help':
+        return 'use:\n   change <name> <old phone> <new phone>\n   or\n   change <name> <field type> <old value> <new value>'
+
+    if len(args) < 3 or len(args) > 4:
+        raise ValueError('incorrect args\nplease enter: change <name> <old phone> <new phone>\n   or\n   change <name> <field type> <old value> <new value>')
+
+    if not isname(args[0]):
+        raise ValueError('incorrect name')
+
+    if len(args) == 3:
+        return contacts.change_record_field(args[0], 'phone', args[1], args[2])
+
     return contacts.change_record_field(*args)
 
 @input_error
 def phone(args_string):
-    args = processing_args(args_string, 1, 1, 'use:\n   phone <name>')
-    return contacts.show(args[0], field_type='phone')
+    if args_string == 'help':
+        return 'use:\n   phone <name>'
+
+    if len(args_string.split()) != 1:
+        raise ValueError('please enter a name')
+
+    name = args_string.strip()
+
+    if not isname(name):
+        raise ValueError('incorrect name')
+
+    return contacts.show(name, field_type='phone')
 
 @input_error
 def show(args_string):
-    args = processing_args(args_string, 1, 2, 'use:\n   show <name>\nor\n   show <name> <field type>')
+    args = args_string.split()
+
+    if args[0] == 'help':
+        return 'use:\n   show <name>\n   or\n   show <name> <field type>'
+
+    if len(args) > 2:
+        raise ValueError('incorrect args\nplease enter:\n   show <name>\n   or\n   show <name> <field type>')
+
+    if not isname(args[0]):
+        raise ValueError('incorrect name')
+
     return contacts.show(*args)
 
 @input_error
 def showall(args_string):
-    args = processing_args(args_string, 0, 0)
+    if args_string:
+        raise ValueError('incorrect args\nuse "show all" without args')
+
     return contacts.showall()
 
 @input_error
 def remove(args_string):
-    args = processing_args(args_string, 1, 3, 'use:\n   remove <name>\n   or\n   remove <name> <field type>\n   or\n   remove <name> <field type> <value>')
+    args = args_string.split()
+
+    if args[0] == 'help':
+        return 'use:\n   remove <name>\n   or\n   remove <name> <field type>\n   or\n   remove <name> <field type> <value> [<value2>, ...]'
+
+    if not isname(args[0]):
+        raise ValueError('incorrect name')
+
+    if len(args) > 3:
+        raise ValueError('incorrect args\nplease enter:\n   remove <name>\n   or\n   remove <name> <field type>\n   or\n   remove <name> <field type> <value> [<value2>, ...]')
 
     if len(args) == 1:
         return contacts.remove_record(args[0])
 
-    if len(args) == 2:
-        return contacts[args[0]].remove_field_type(args[1])
+    try:
+        if len(args) == 2:
+            return contacts[args[0]].remove_field_type(args[1])
 
-    if len(args) == 3:
-        return contacts[args[0]].remove_field(args[1], args[2])
+        if len(args) == 3:
+            return contacts[args[0]].remove_field(args[1], args[2])
+    except KeyError:
+        raise KeyError(f'user {args[0]} not found')
 
 @input_error
 def quit(args_string):
-    args = processing_args(args_string, 0, 0)
+    if args_string:
+        raise ValueError('incorrect args\nuse "exit" without args')
     return 'Good bye!'
 
 @input_error
 def birthday(args_string):
-    args = processing_args(args_string, 1, 1, 'use: birthday <name>')
+    args = args_string.split()
     return contacts[args[0]].days_to_birthday()
 
+def check_input(string):
+    #return True
+    return not re.search(r'[^A-Za-z0-9@\-\.\,\s\+]', string)
+
+def isname(string):
+    return not re.search(r'[^A-Za-z]', string)
+
+#def isphone(string):
+#    return not re.search(r'[^0-9]', sanitize_phone_number(string))
+
+@input_error
 def parse_input(string):
-    if re.search(r'[^A-Za-z0-9@\-\.\,\s\+]', string):
+
+    if not check_input(string):
         raise ValueError('"'+string+'" not a valid command\nunsupported chars') 
     result = re.sub(r'[\s\,]+', ' ', string.strip())
 
@@ -276,7 +369,9 @@ def parse_input(string):
             return (command, result[len(command):])
 
     raise ValueError(result.split()[0]+' not a valid command')
-
+    
+#def get_record(name):
+#    return USERS_DICT.get(name) or f'user not found: {name}'
 
 def add_default_records():
     records = ['add Ivan phone 0551112233 0320112299 0118885566',
@@ -286,7 +381,7 @@ def add_default_records():
     'add Kate email kate@gmail.com',
     'add Kate birthday 23-09-2004',
     'add Kate note somethingabout',
-    'add Max phone 0321515822',
+    'add Max 0321515822',
     'add Max email max@gmail.com',
     'add Krysta phone 0912225566 0935556667 0995812244',
     'add Krysta birthday 09-03-1980']
@@ -295,7 +390,11 @@ def add_default_records():
         FUNC_DICT[command[0]](command[1])
 
 def main():
+    #test
     add_default_records()
+    #for a in contacts.iterator(5):
+    #    print(a)
+
     while True:
         user_input = input('>>> ')
         if user_input ==  '.':
@@ -310,10 +409,13 @@ def main():
             if result == 'Good bye!':
                 exit()
 
+#FUNC_DICT <call name>: (func name, args amount, multiple value allow, help string)
 FUNC_DICT=  {'hello': hello, 'add': add, 'change': change, 'phone': phone,
             'show all': showall, 'exit': quit, 'good bye': quit, 'close': quit,
             'show': show, 'remove': remove, 'birthday': birthday}
+FUNC_ARGS=dict()
 contacts = AddressBook(dict())
 
 if __name__ == '__main__':
+
     main()
